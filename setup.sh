69c9f806059758d663850cc46f18b6c59bb7f78d
Ainsof So'o <ainsof.soo@gmail.com>
2024-08-08 02:43:35 +0000

    wip

--- setup.sh
+++ setup.sh
@@ -3,14 +3,14 @@ set -e
 
 # Create new drupal core code tree
 mkdir drupal
-composer -n create-project drupal/recommended-project:${DRUPAL_CORE_VERSION} drupal
+# composer -n create-project drupal/recommended-project:${DRUPAL_CORE_VERSION} drupal
+composer -n create-project --repository-url=https://github.com/ainsofs/drupal-project/tree/10.3-ainsofs drupal
 cd drupal
 
 # Add essential contirbute modules
 composer -n require drush/drush
-composer -n require drupal/admin_toolbar
-composer -n require 'drupal/gin:^3.0@RC'
 composer -n require 'drupal/project_browser:^1.0@beta'
+composer -n require 'ainsofs/drupal-base'
 
 # Add developer modules
 composer -n require drupal/twig_debugger --dev
@@ -25,16 +25,18 @@ vendor/bin/phpcs --config-set default_standard Drupal,DrupalPractice
 
 # Install Drupal
 vendor/bin/drush -y site:install \
-  standard \
+  minimal \
   --site-name="gitpod-drupal" \
   --account-name="admin" \
   --account-pass="$(openssl rand -base64 16)" \
   --db-url=sqlite://sites/default/files/.ht.sqlite
 
+# Install recipe
+cd drupal/
+php web/scripts/drupal recipes recipes/contrib/drupal-base
+
 # Enable themes and modules
-vendor/bin/drush -y theme:enable gin
-vendor/bin/drush -y config:set system.theme admin gin
-vendor/bin/drush -y en admin_toolbar project_browser
+# vendor/bin/drush -y en project_browser
 
 # Install aliases
 curl -L https://gist.github.com/ainsofs/ba947741b230606be5d2f4aad6faf7bf/raw -o .bash_aliases
